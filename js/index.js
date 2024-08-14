eb67ef6146cb2981692fc736f9d690c2f084d183
datgggg <dathoangsd@gmail.com>
2024-08-14 09:23:27 +0700

    upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Duy Dat co cai nay muon noi voi Lquynh ne:3'.split('')
+const text = 'T co cai nay muon noi voi m ne:3'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I miss you, can you give me another chance to be your lover').split('')
+    const titles = ('m la con duy khung dien:))').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
